ab47e4008df806b56fd0cf8a2d22d37a8ef9ceaa
okan bozkurt <okanbozkurt@usta.com.tr>
2021-11-12 16:29:02 +0300

    update on layout

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -8,6 +8,7 @@ export default () => {
   return (
     <BrowserRouter>
       <div>
+        <p>cicd test</p>
         <ShowMessage></ShowMessage>
         <Header></Header>
         <MarketingApp></MarketingApp>
